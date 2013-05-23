9aeeb0b2423ade90c20cd1708257fcf311077389
Mell Zamora <mellzamora@outlook.com>
2013-05-23 09:15:28 +0800

    added license

--- Block/Service/RssBlockService.php
+++ Block/Service/RssBlockService.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RzBlockBundle package.
+ *
+ * (c) mell m. zamora <mell@rzproject.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rz\BlockBundle\Block\Service;
 
 use Sonata\BlockBundle\Block\Service\RssBlockService as BaseBlockService;
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RzBlockBundle package.
+ *
+ * (c) mell m. zamora <mell@rzproject.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rz\BlockBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
